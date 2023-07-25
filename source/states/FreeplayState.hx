dea9e0b81b5af41a37a39a45655e042c90ff2f2c
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-25 11:34:41 +0800

    Add files via upload

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -384,7 +384,7 @@ class FreeplayState extends MusicBeatState
 			FlxG.sound.music.volume = 0;
 					
 			destroyFreeplayVocals();
-			#if MODS_ALLOWED
+			#if desktop
 			DiscordClient.loadModRPC();
 			#end
 		}
