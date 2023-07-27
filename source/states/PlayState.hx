159f560278cfb07f096276d9b71dac674746c62e
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-27 15:38:20 +0800

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -1550,7 +1550,7 @@ class PlayState extends MusicBeatState
 			botplayTxt.alpha = 1 - Math.sin((Math.PI * botplaySine) / 180);
 		}
 
-		if (controls.PAUSE && startedCountdown && canPause)
+		if (controls.PAUSE #if android || FlxG.android.justReleased.BACK #end && startedCountdown && canPause)
 		{
 			var ret:Dynamic = callOnLuas('onPause', null, true);
 			if(ret != FunkinLua.Function_Stop) {
