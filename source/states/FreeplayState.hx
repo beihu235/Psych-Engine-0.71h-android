ba4b30d709952a6b484337acb5b5311e990038f3
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-27 19:08:59 +0800

    Add files via upload

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -406,6 +406,9 @@ class FreeplayState extends MusicBeatState
 		}
 		else if(controls.RESET #if android || _virtualpad.buttonY.justPressed #end)
 		{
+		    #if android
+			removeVirtualPad();
+			#end
 			persistentUpdate = false;
 			openSubState(new ResetScoreSubState(songs[curSelected].songName, curDifficulty, songs[curSelected].songCharacter));
 			FlxG.sound.play(Paths.sound('scrollMenu'));
