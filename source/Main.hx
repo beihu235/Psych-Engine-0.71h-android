f1e96290e1b1bf97d42c972830173729d05650f2
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-28 15:26:49 +0800

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -89,7 +89,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
-		#if !mobile
+	
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -97,7 +97,7 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.data.showFPS;
 		}
-		#end
+		
 
 		#if html5
 		FlxG.autoPause = false;
