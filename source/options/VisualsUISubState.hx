216778c34417a33761408c84a4cb34609b5d4b58
beihu235 <105789304+beihu235@users.noreply.github.com>
2023-07-28 15:27:16 +0800

    Add files via upload

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -113,14 +113,14 @@ class VisualsUISubState extends BaseOptionsMenu
 		option.decimals = 1;
 		addOption(option);
 		
-		#if !mobile
+		
 		var option:Option = new Option('FPS Counter',
 			'If unchecked, hides FPS Counter.',
 			'showFPS',
 			'bool');
 		addOption(option);
 		option.onChange = onChangeFPSCounter;
-		#end
+		
 		
 		var option:Option = new Option('Pause Screen Song:',
 			"What song do you prefer for the Pause Screen?",
