1c1bf427fe8d219a7d6e82d0fb9831da8cff8147
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-07-29 15:12:21 +0800

    Add files via upload

--- source/psychlua/ExtraFunctions.hx
+++ source/psychlua/ExtraFunctions.hx
@@ -98,7 +98,7 @@ class ExtraFunctions
 				case 'down': return PlayState.instance.controls.NOTE_DOWN_P;
 				case 'up': return PlayState.instance.controls.NOTE_UP_P;
 				case 'right': return PlayState.instance.controls.NOTE_RIGHT_P;
-				case 'space': return PlayState.instance.controls.SPACE_P;
+				case 'space': key = (PlayState.instance.getControl.SPACE_P || FlxG.keys.justPressed.SPACE);//an extra key for convinience
 				default: return PlayState.instance.controls.justPressed(name);
 			}
 			return false;
@@ -109,7 +109,7 @@ class ExtraFunctions
 				case 'left': return PlayState.instance.controls.NOTE_LEFT;
 				case 'down': return PlayState.instance.controls.NOTE_DOWN;
 				case 'up': return PlayState.instance.controls.NOTE_UP;
-				case 'space': return PlayState.instance.controls.SPACE;
+				case 'space': key = (PlayState.instance.getControl.SPACE || FlxG.keys.pressed.SPACE);//an extra key for convinience
 				default: return PlayState.instance.controls.pressed(name);
 			}
 			return false;
@@ -120,7 +120,7 @@ class ExtraFunctions
 				case 'left': return PlayState.instance.controls.NOTE_LEFT_R;
 				case 'down': return PlayState.instance.controls.NOTE_DOWN_R;
 				case 'up': return PlayState.instance.controls.NOTE_UP_R;
-				case 'space': return PlayState.instance.controls.SPACE_R;
+				case 'space': key = (PlayState.instance.getControl.SPACE_R || FlxG.keys.justReleased.SPACE);//an extra key for convinience
 				default: return PlayState.instance.controls.justReleased(name);
 			}
 			return false;
