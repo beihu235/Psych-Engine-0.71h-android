5e608c6345492123d66a346385d0ceb14e1d5688
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-07-30 19:03:37 +0800

    Add files via upload

--- source/backend/Mods.hx
+++ source/backend/Mods.hx
@@ -88,7 +88,7 @@ class Mods
 
 		for (file in paths)
 		{
-			var list:Array<String> = CoolUtil.coolTextFile(file, false);
+			var list:Array<String> = CoolUtil.coolTextFile(file);
 			for (value in list)
 				if((allowDuplicates || !mergedList.contains(value)) && value.length > 0)
 					mergedList.push(value);
