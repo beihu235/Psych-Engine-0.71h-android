d9edf22710469453a71ec3b8dc43e314afb78a55
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-07-30 19:25:35 +0800

    Add files via upload

--- source/states/ModsMenuState.hx
+++ source/states/ModsMenuState.hx
@@ -453,7 +453,7 @@ class ModsMenuState extends MusicBeatState
 
 		var path:String = SUtil.getPath() + 'modsList.txt';
 		File.saveContent(path, fileStr);
-		Paths.pushGlobalMods();
+		Mods.pushGlobalMods();
 	}
 
 	var noModsSine:Float = 0;
