35e1480442de0d48b25f7b3eccc33ee46a7e688b
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-03 20:12:05 +0800

    Add files via upload

--- source/backend/AndroidDialogsExtend.hx
+++ source/backend/AndroidDialogsExtend.hx
@@ -11,7 +11,71 @@ class AndroidDialogsExtend{
         AndroidDialogs.ShowToast(showtext, AndroidDialogs.LENGTH_LONG);//or LENGTH_SHORT duration
     }
 
+    public static function OpenAlert(Title:String, Message:String, ConfirmName:String, CancelName:String)
+    {
+        /*
+            param: Title dialog.
+            param: Message dialog.
+            param: Caption confirm button dialog.
+            param: Caption cancel button dialog.
+        */
+        AndroidDialogs.ShowAlertDialog(Title, Message, ConfirmName, CancelName);
+    }
+    /*
+    function OtherFunctionGetResult():Void
+    {
+        //get string user selection
+        trace(AndroidDialogs.objHaxe.answerDialog);
+    }
+    */ //use AndroidDialogs.objHaxe.answerDialog get data
+    
+    public static function OpenAlertSelect(Title:String, choose1:String, choose2:String, choose3:String, choose4:String, choose5:String)
+    {
+        var names_players:Array<String> = new Array<String>();
+        names_players.push(choose1);
+        if (choose2 != null) names_players.push(choose2);
+        if (choose3 != null) names_players.push(choose3);
+        if (choose4 != null) names_players.push(choose4);
+        if (choose5 != null) names_players.push(choose5);
+        /*
+            param: Title dialog.
+            param: Array String values
+        */
+        AndroidDialogs.ShowAlertSelectOption(Title, names_players);
+    }
+    /*
+    function OtherFunctionGetResult():Void
+    {
+        //get string user selection
+        trace(AndroidDialogs.objHaxe.answerOptionSelected);
+    }
+    */ //use AndroidDialogs.objHaxe.answerOptionSelected get data
+    
+    /*
+    function AnyFuntion():Void
+    {
+        var names_players:Array<String> = new Array<String>();
+        names_players.push("Pity Martinez");
+        names_players.push("Jonathan Maidana");
+        names_players.push("Enzo Perez");
+        names_players.push("Ignacio Scoco");
+        names_players.push("Leonardo Ponzio");
+        /*
+            param: Title dialog.
+            param: Array String values
+        
+        AndroidDialogs.ShowAlertMultipleSelectOption("Title", names_players);
+    }
 
-
-
+    function OtherFunctionGetResult():Void
+    {
+        //get value boolean user selection
+        for (valueBoolean in AndroidDialogs.objHaxe.answersOptionsMultipleSelected)
+        {
+            trace("FLAG: " + valueBoolean);
+        }
+    }
+    */
 }
+
+
