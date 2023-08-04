325adb9e17a4dbc89042c78ff8c826f6a9fbaf2f
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-04 17:36:41 +0800

    Add files via upload

--- source/cutscenes/DialogueBoxPsych.hx
+++ source/cutscenes/DialogueBoxPsych.hx
@@ -164,7 +164,7 @@ class DialogueBoxPsych extends FlxSpriteGroup
 			bgFade.alpha += 0.5 * elapsed;
 			if(bgFade.alpha > 0.5) bgFade.alpha = 0.5;
 
-			if(Controls.instance.ACCEPT) {
+			if(FlxG.keys.justPressed.ESCAPE #if android || justTouched #end) {
 				if(!daText.finishedText) {
 					daText.finishText();
 					if(skipDialogueThing != null) {
