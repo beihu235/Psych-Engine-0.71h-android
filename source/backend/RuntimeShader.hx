76983b54a927e1b55efe302c30d4bc7adba05335
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-06 21:48:37 +0800

    Add files via upload

---
+++ source/backend/RuntimeShader.hx
@@ -0,0 +1,9 @@
+package backend;
+
+class RuntimeShader extends FlxShader {
+     public function new(__glFragmentSource:String, __glVertexSource:String) {
+         this.__glFragmentSource = __glFragmentSource;
+         this.__glVertexSource = __glVertexSource;
+         super();
+     }
+}
