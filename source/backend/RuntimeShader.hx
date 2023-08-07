e37739f64adc0edf631721b77e906625a2f13438
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-07 19:52:36 +0800

    Delete RuntimeShader.hx

--- source/backend/RuntimeShader.hx
+++
@@ -1,9 +0,0 @@
-package backend;
-
-class RuntimeShader extends FlxShader {
-     public function new(__glFragmentSource:String, __glVertexSource:String) {
-         this.__glFragmentSource = __glFragmentSource;
-         this.__glVertexSource = __glVertexSource;
-         super();
-     }
-}
