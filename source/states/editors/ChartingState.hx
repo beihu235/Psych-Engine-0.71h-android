87d61e130a4f1cef64e84c4b1f0ad1da2073ee69
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-09 10:07:02 +0800

    Add files via upload

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -1935,7 +1935,7 @@ class ChartingState extends MusicBeatState
 				var holdingShift:Float = 1;
 				if (FlxG.keys.pressed.CONTROL) holdingShift = 0.25;
 				else if (FlxG.keys.pressed.SHIFT #if android || MusicBeatState._virtualpad.buttonC.pressed #end) holdingShift = 4;
-                else if (_virtualpad.buttonX.pressed) holdingShift = 10;
+                else if (MusicBeatState._virtualpad.buttonX.pressed) holdingShift = 10;
 				var daTime:Float = 700 * FlxG.elapsed * holdingShift;
 
 				if (FlxG.keys.pressed.W #if android || MusicBeatState._virtualpad.buttonUp.pressed #end)
