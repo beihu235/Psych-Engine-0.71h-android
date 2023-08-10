a5c4d7ce161f04eb53f46eff5669fb16cd415d01
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-10 11:07:32 +0800

    Add files via upload

--- source/android/FlxVirtualPad.hx
+++ source/android/FlxVirtualPad.hx
@@ -196,8 +196,8 @@ class FlxVirtualPad extends FlxSpriteGroup {
 				dPad.add(add(buttonCEDown = createButton(FlxG.width - (44 + 42 * 4) * 3, FlxG.height - 45 * 3, 44 * 3, 127, "down", 0x00FFFF)));		
 				dPad.add(add(buttonCEG = createButton(FlxG.width - (44 + 42 * 1) * 3, 25, 44 * 3, 127, "g", 0x00FF00)));
 			case MenuCharacterEditor:
-				actions.add(add(buttonA = createButton(FlxG.width - 44 * 3 - 320, FlxG.height - 45 * 3, 44 * 3, 127, "a", 0xFF0000)));
-				actions.add(add(buttonB = createButton(FlxG.width - 44 * 3 - 320, FlxG.height - 85 * 3, 44 * 3, 127, "b", 0xFFCB00)));	
+				actions.add(add(buttonA = createButton(FlxG.width - 44 * 3 - 335, FlxG.height - 45 * 3, 44 * 3, 127, "a", 0xFF0000)));
+				actions.add(add(buttonB = createButton(FlxG.width - 44 * 3 - 335, FlxG.height - 85 * 3, 44 * 3, 127, "b", 0xFFCB00)));	
 			case NOTESTATE:
 			    actions.add(add(buttonE = createButton(42 * 3, FlxG.height - 125 * 3, 44 * 3, 127, "e", 0xFF7D00)));
 				actions.add(add(buttonC = createButton(0, FlxG.height - 125 * 3, 44 * 3, 127, "c", 0x44FF00)));  
