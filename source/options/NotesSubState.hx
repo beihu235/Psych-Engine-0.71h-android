878958bb459b2d0d06a41d259961929bf55e49a7
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-15 15:45:00 +0800

    Add files via upload

--- source/options/NotesSubState.hx
+++ source/options/NotesSubState.hx
@@ -169,7 +169,7 @@ class NotesSubState extends MusicBeatSubstate
 		_lastControllerMode = controls.controllerMode;
 		
 		
-		UI_songTitle = new FlxUIInputText(300, 10, 70, alphabetHex.text, 8);
+		UI_songTitle = new FlxUIInputText(300, 300, 50, '', 8);
 		UI_songTitle.focusGained = () -> FlxG.stage.window.textInputEnabled = true;
 		//blockPressWhileTypingOn.push(UI_songTitle);
 		LengthCheck = UI_songTitle.text;
@@ -440,7 +440,7 @@ class NotesSubState extends MusicBeatSubstate
 			else if(pointerY() >= hexTypeLine.y && pointerY() < hexTypeLine.y + hexTypeLine.height &&
 					Math.abs(pointerX() - 1000) <= 84)
 			{
-			    FlxG.stage.window.textInputEnabled = true;
+			    //FlxG.stage.window.textInputEnabled = true;
 				hexTypeNum = 0;
 				for (letter in alphabetHex.letters)
 				{
