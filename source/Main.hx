ac8b8a06cd4961cbb4dbfb8d833ce4c6a36a8055
北狐丶逐梦 <105789304+beihu235@users.noreply.github.com>
2023-08-20 11:04:19 +0800

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -83,13 +83,14 @@ class Main extends Sprite
 			game.width = Math.ceil(stageWidth / game.zoom);
 			game.height = Math.ceil(stageHeight / game.zoom);
 		}
-		SUtil.doTheCheck();
+		
 	
 		#if LUA_ALLOWED Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
+        SUtil.doTheCheck();
 		
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
